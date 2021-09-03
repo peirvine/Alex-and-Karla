0f73c2f6c534eefba78215aa00b4b163ffa92e9d
Peter Irvine <peirvine@gmail.com>
2021-09-03 14:09:46 -0500

    feat: initial theme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Nav from './components/nav/nav'
+import Home from './components/home/home'
+import Story from './components/story/story'
+import Schedule from './components/schedule/schedule'
+import Travel from './components/travel/travel'
+import Registry from './components/registry/registry'
+import WeddingParty from './components/weddingParty/weddingParty'
+import Photos from './components/photos/photos'
+import Footer from './components/footer/footer'
+
 import './App.scss'
-import proposalJ from './images/proposal.jpg'
-import proposalW from './images/proposal.webp'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <picture>
-        <source srcset={proposalW} type="image/webp" />
-        <source srcset={proposalJ} type="image/jpeg" /> 
-        <img src={proposalJ} alt="Alt Text!" />
-      </picture>
+      <Router>
+        <Nav />
+          <Switch>
+            <Route path="/story">
+              <Story />
+            </Route>
+            <Route path="/schedule">
+              <Schedule />
+            </Route>
+            <Route path="/travel">
+              <Travel />
+            </Route>
+            <Route path="/registry">
+              <Registry />
+            </Route>
+            <Route path="/wedding-party">
+              <WeddingParty />
+            </Route>
+            <Route path="/photos">
+              <Photos />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        <Footer />
+      </Router>
     </div>
   )
 }
---
+++ src/components/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './footer.scss'
+
+import footerP from '../../images/footer.png'
+
+export default function Footer() {
+  return (
+    <div id="footer">
+      {/* <img src={footerP} alt="footer mountains" /> */}
+    </div>
+  )
+}
