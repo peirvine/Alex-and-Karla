67b6c8c84f41162c116899ff238873237854b852
Peter Irvine <peirvine@gmail.com>
2021-12-28 18:29:55 -0600

    desktop mode done

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -7,13 +7,13 @@ import ourAdventure from '../../images/Adventure.png'
 export default function Home () {
   return ( 
     <div className="home">
-      <div className="homeText"> 
+      <div id="homeText"> 
         <picture>
-        <img width="400" id="ouradventure" src={ourAdventure} alt="Alt Text!" />
-      </picture>
-      <h1>Alex and Karla</h1>
-      <h3>July 16, 2022</h3>
-      <h3>Rush City, MN</h3>
+          <img width="400" id="ouradventure" src={ourAdventure} alt="Alt Text!" />
+        </picture>
+        <h1>Alex and Karla</h1>
+        <h2>Rush City, MN</h2>
+        <h3>July 16, 2022</h3>
       </div>
       <picture>
         <img id="homepic" src={proposalJ} alt="Alt Text!" />
