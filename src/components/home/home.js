599a3225092681f62e78fc4bcf6845d9d08b020f
Peter Irvine <peirvine@gmail.com>
2022-05-17 22:09:56 -0500

    working copy

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,17 +4,20 @@ import proposalJ from '../../images/homepage.jpg'
 import ourAdventure from '../../images/Adventure.png'
 
 export default function Home () {
-  return ( 
+  return (
     <div className="home">
-      <div id="homeText"> 
+      <div id="homeText">
         <a href="/">
           <img id="ouradventure" src={ourAdventure} alt="Alex and Karla - Our Adventure Begins" />
         </a>
         <h1>Alex and Karla</h1>
         <h2>Rush City, MN</h2>
         <h3>July 16, 2022</h3>
+        <div id="rsvpHold">
+          <a id="homeRsvp" href="https://alexandkarla.anrsvp.com/" rel="noreferrer" target="_blank">RSVP</a>
+        </div>
       </div>
       <img id="homepic" src={proposalJ} alt="proposal" />
     </div>
-   )
+  )
 }
