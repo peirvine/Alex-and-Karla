80f03650b93bd5495477b9ceff5870d937ed1a76
Peter Irvine <peirvine@gmail.com>
2022-05-17 22:59:48 -0500

    update desktop wedding party

--- src/components/weddingParty/weddingPeople.js
+++ src/components/weddingParty/weddingPeople.js
@@ -12,10 +12,10 @@ export const weddingParty = [
   {
     brideName: "KAYLA JOOS - MAID OF HONOR",
     brideImage: kayla,
-    brideBlurb: "",
+    brideBlurb: "Bacon ipsum dolor amet brisket chislic pastrami, doner jerky biltong venison pork. Pork chop prosciutto doner tenderloin pork belly, hamburger drumstick tail. Cupim cow kielbasa chuck shankle porchetta. Meatloaf pancetta shank, pork loin sausage shankle doner ribeye cow t-bone. Doner short ribs shankle, turkey sausage prosciutto biltong bacon burgdoggen ball tip ham frankfurter. Sirloin ground round filet mignon ham, leberkas drumstick cupim. Brisket ribeye shoulder sirloin ball tip boudin chislic tri-tip. Drumstick shank pastrami capicola. Kevin chicken chuck fatback flank. Brisket short loin meatball, landjaeger porchetta jowl pastrami rump pork chop ribeye ball tip boudin kielbasa. Pastrami alcatra pig sirloin doner ham chislic porchetta. Buffalo bresaola tri-tip, ribeye doner kielbasa chuck swine shoulder short ribs drumstick pork belly.",
     groomName: "PETER IRVINE - BEST MAN",
     groomImage: peter,
-    groomBlurb: "",
+    groomBlurb: "Bacon ipsum dolor amet brisket chislic pastrami, doner jerky biltong venison pork. Pork chop prosciutto doner tenderloin pork belly, hamburger drumstick tail. Cupim cow kielbasa chuck shankle porchetta. Meatloaf pancetta shank, pork loin sausage shankle doner ribeye cow t-bone. Doner short ribs shankle, turkey sausage prosciutto biltong bacon burgdoggen ball tip ham frankfurter. Sirloin ground round filet mignon ham, leberkas drumstick cupim. \n\n Brisket ribeye shoulder sirloin ball tip boudin chislic tri-tip. Drumstick shank pastrami capicola. Kevin chicken chuck fatback flank. Brisket short loin meatball, landjaeger porchetta jowl pastrami rump pork chop ribeye ball tip boudin kielbasa. Pastrami alcatra pig sirloin doner ham chislic porchetta. Buffalo bresaola tri-tip, ribeye doner kielbasa chuck swine shoulder short ribs drumstick pork belly.",
   },
   {
     brideName: "MANDY JAHNER",
