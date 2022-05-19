95f0d3e44c79d8c3954221fd0729633ca886afa2
Alex Oelke <alex@oelke.com>
2022-05-18 21:56:45 -0500

    Adding randomly found wedding party photos for those who did not provide one

--- src/components/weddingParty/weddingPeople.js
+++ src/components/weddingParty/weddingPeople.js
@@ -7,17 +7,20 @@ import alexis from "../../images/weddingParty/Alexis.png"
 import kayla from "../../images/weddingParty/Kayla.png"
 
 import peter from "../../images/weddingParty/peter.jpg"
-import lane from '../../images/weddingParty/lane.jpg'
+import lane from "../../images/weddingParty/lane.jpg"
+import matt from "../../images/weddingParty/Matt.jpg"
+import ethan from "../../images/weddingParty/Ethan.jpg"
+import nate from "../../images/weddingParty/Nate.jpg"
 
 export const weddingParty = [
   {
     brideName: "KAYLA JOOS - MAID OF HONOR",
     brideImage: kayla,
-    brideBlurb: "Bacon ipsum dolor amet brisket chislic pastrami, doner jerky biltong venison pork. Pork chop prosciutto doner tenderloin pork belly, hamburger drumstick tail. Cupim cow kielbasa chuck shankle porchetta. Meatloaf pancetta shank, pork loin sausage shankle doner ribeye cow t-bone. Doner short ribs shankle, turkey sausage prosciutto biltong bacon burgdoggen ball tip ham frankfurter. Sirloin ground round filet mignon ham, leberkas drumstick cupim. Brisket ribeye shoulder sirloin ball tip boudin chislic tri-tip. Drumstick shank pastrami capicola. Kevin chicken chuck fatback flank. Brisket short loin meatball, landjaeger porchetta jowl pastrami rump pork chop ribeye ball tip boudin kielbasa. Pastrami alcatra pig sirloin doner ham chislic porchetta. Buffalo bresaola tri-tip, ribeye doner kielbasa chuck swine shoulder short ribs drumstick pork belly.",
+    brideBlurb: "",
     groomName: "PETER IRVINE - BEST MAN",
     groomImage: peter,
-    groomBlurb: "Bacon ipsum dolor amet brisket chislic pastrami, doner jerky biltong venison pork. Pork chop prosciutto doner tenderloin pork belly, hamburger drumstick tail. Cupim cow kielbasa chuck shankle porchetta. Meatloaf pancetta shank, pork loin sausage shankle doner ribeye cow t-bone. Doner short ribs shankle, turkey sausage prosciutto biltong bacon burgdoggen ball tip ham frankfurter. Sirloin ground round filet mignon ham, leberkas drumstick cupim. \n\n Brisket ribeye shoulder sirloin ball tip boudin chislic tri-tip. Drumstick shank pastrami capicola. Kevin chicken chuck fatback flank. Brisket short loin meatball, landjaeger porchetta jowl pastrami rump pork chop ribeye ball tip boudin kielbasa. Pastrami alcatra pig sirloin doner ham chislic porchetta. Buffalo bresaola tri-tip, ribeye doner kielbasa chuck swine shoulder short ribs drumstick pork belly.",
-  },
+    groomBlurb: "" 
+   },
   {
     brideName: "MANDY JAHNER",
     brideImage: mandy,
@@ -31,7 +34,7 @@ export const weddingParty = [
     brideImage: alexis,
     brideBlurb: "",
     groomName: "NATHAN FAUST",
-    groomImage: tgt,
+    groomImage: nate,
     groomBlurb: "",
   },
   {
@@ -39,7 +42,7 @@ export const weddingParty = [
     brideImage: elise,
     brideBlurb: "",
     groomName: "ETHAN SCHURTER",
-    groomImage: tgt,
+    groomImage: ethan,
     groomBlurb: "",
   },
   {
@@ -47,7 +50,7 @@ export const weddingParty = [
     brideImage: abbey,
     brideBlurb: "",
     groomName: "MATT VRCHOTA",
-    groomImage: tgt,
+    groomImage: matt,
     groomBlurb: "",
   },
 ]
