9fc4744ab1fac24e633e28febb155d65b7a4990c
Alex Oelke <alex@oelke.com>
2022-05-20 11:50:42 -0500

    Replace BestWestern webp with png because steve jobs. Adding phone number for americinn. Fix for footer

--- src/components/travel/travel.js
+++ src/components/travel/travel.js
@@ -1,5 +1,6 @@
-import BWP from "../../images/Hotels/BestWestern.webp"
+import BWP from "../../images/Hotels/BestWestern.png"
 import AI from "../../images/Hotels/AmericInn.png"
+import './travel.scss'
 export default function Travel () {
   return (
     <div className="wrapper">
@@ -10,7 +11,7 @@ export default function Travel () {
       <div id="images">
         <a href="https://www.bestwestern.com/en_US/book/hotels-in-isanti/best-western-plus-isanti/propertyCode.24145.html" target="_blank" rel="noreferrer"><img src={BWP} alt="BestWesternPlus" /></a>
           <a href="https://www.wyndhamhotels.com/americinn/north-branch-minnesota/americinn-lodge-and-suites-north-branch/overview" target="_blank" rel="noreferrer"><img src={AI} alt="americinn" /></a>
-          <p> A block of rooms has been reserved at the AmericInn in North Branch. <br></br> To reserve please call them directly and reference the Oelke / Koppendrayer wedding.</p>
+          <p> A block of rooms has been reserved at the AmericInn in North Branch. <br></br> To reserve please call them directly at 800-634-3444 and reference the Oelke / Koppendrayer wedding.</p>
       </div>
     </div>
   )
